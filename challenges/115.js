de3a5cd06edc14da05e10246caa7611a1c445558
Sophia <sophiar.rafat@gmail.com>
2021-02-05 05:44:24 -0600

    git commit -m"complete challenge"

--- challenges/115.js
+++ challenges/115.js
@@ -20,14 +20,14 @@
 // Example
 //
 // // ```
-// let items = [
-//   { name: 'Soy milk', expiryDate: '2021-01-01' },
-//
-//   { name: 'Sirloin', expiryDate: '2021-01-05' },
-//
-//   { name: 'Chewy lentils', expiryDate: '2021-02-28' }
-//
-// ];
+let items = [
+  { name: 'Soy milk', expiryDate: '2021-01-01' },
+
+  { name: 'Sirloin', expiryDate: '2021-01-05' },
+
+  { name: 'Chewy lentils', expiryDate: '2021-02-28' }
+
+];
 
 // cleanOutFridge(items);
 //
@@ -36,9 +36,39 @@
 // // ['Soy milk', 'Sirloin']
 
 
-
 function cleanOutFridge(array){
-  return // your code here
+	//We will push expired foods into this empty array
+	let toToss = [];
+	//This variable will help us convert the date back into days
+	let oneDay = 1000*60*60*24;//I don't fully understand the concept here
+for (let foods in array){
+	// Convert both dates to milliseconds
+	let today = new Date();
+	let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+	let expDate = new Date(items[foods].expiryDate);
+  // Calculate the difference in milliseconds
+  let diffDate = today - expDate;
+	//This variable will help us convert the date back into days
+	let oneDay = 1000*60*60*24;//I don't fully understand the concept here
+ // Convert back to days and return
+  let diffDate1 = Math.round(diffDate/oneDay);
+	//Print days past expiration date for each item
+	if (diffDate1 <= 7){
+	 console.log(`The ${array[foods].name} is not expired.`);
+	} else {
+  console.log(`The ${array[foods].name} is ${diffDate1} days beyond its expiration date.`);
+	}
+	//If the difference between today's date and the foods' expiration date is greater than 7, push food into toToss array 
+  if( today - expDate >= 7){
+		toToss.push(items[foods]);
+	}
+ }
+ return toToss;
 }
+
+cleanOutFridge(items);
+
+
+
 
 module.exports = cleanOutFridge;
